fccd34ba71bea533e0a5b69838296af3cd5122f4
HeroBuxx <me@herobuxx.me>
2023-04-13 23:33:49 +0000

    sdm660-common: Switch to QTI USB 1.3
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- sdm660.mk
+++ sdm660.mk
@@ -414,6 +414,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
 
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service-qti
+
 # Vibrator
 PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl:64 \
